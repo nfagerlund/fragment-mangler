8623f3730004d77096b784212ccb77d53287119b
Nick Fagerlund <nick.fagerlund@gmail.com>
2015-07-24 16:06:50 -0700

    Extract segmenter call into a method, for easier replacement later

--- sentence_segmenter.rb
+++ sentence_segmenter.rb
@@ -1,14 +1,20 @@
 require 'nokogiri'
 require 'punkt-segmenter'
 
+@tokenizer = nil
+
+def call_punkt_segmenter(text)
+  @tokenizer.sentences_from_text(text, :output => :sentences_text)
+end
+
 def segment_on_sentences(text)
   parsed = Nokogiri::HTML::DocumentFragment.parse(text)
-  tokenizer = Punkt::SentenceTokenizer.new(text)
+  @tokenizer = Punkt::SentenceTokenizer.new(text)
 
   all_paragraphs = parsed.css('p')
 
   all_paragraphs.each do |graf|
-    sentences = tokenizer.sentences_from_text(graf.inner_html, :output => :sentences_text)
+    sentences = call_punkt_segmenter(graf.inner_html)
     new_div = '<div class="real-paragraph"> <p class="temp-sentence">' << sentences.join('</p> <p class="temp-sentence">') << '</p></div>'
     graf.replace(new_div)
   end
